97f1b0156ee58d9fc555623bd1cc656bc09ec245
basma <bassmaeljazzouli@gmail.com>
2023-09-19 09:52:36 +0100

    challenge1

---
+++ array_challenge1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main(){
+
+    int tab[] = {1,2,3,4,5,6,7,8,9,10};  
+
+    printf("%d\n", tab[0]);
+    printf("%d\n", tab[1]);
+    printf("%d\n", tab[2]);
+    printf("%d\n", tab[3]);
+    printf("%d\n", tab[4]);
+    printf("%d\n", tab[5]);
+    printf("%d\n", tab[6]);
+    printf("%d\n", tab[7]);
+    printf("%d\n", tab[8]);w<
+    printf("%d\n", tab[9]);
+  
+    
+
+
+
+
+
+return 0;
+}
